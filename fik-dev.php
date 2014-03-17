7957eb021409c66c167724b3d11d57d375f94ac2
Bill Murphy <murphy@litio.net>
2014-03-17 17:34:51 +0100

    Add thumbnail support and echo the_ functions

--- fik-dev.php
+++ fik-dev.php
@@ -58,13 +58,79 @@ add_action( 'init', 'product_post_type', 0 );
 
 
 function the_fik_price(){
-	return '<span itemprop="price" class="price"><span class="amount">109,99</span> EUR</span>';
+	echo('<span itemprop="price" class="price"><span class="amount">109,99</span> EUR</span>');
+	return;
 
 }
 
 function the_fik_add_to_cart_button(){
-	return '<form action="" class="fik_add_cart" method="post" enctype="multipart/form-data"><input type="hidden" name="store_product_id" value="38"><div class="control-group product-quantity"><label class="control-label" for="quantity">Quantity</label><div class="controls"><input type="number" name="quantity" class="input-mini" min="1" max="10" step="1" value="1" required=""></div></div><button type="submit" class="button alt btn btn-primary">Add to cart</button></form>';
+	echo('<form action="" class="fik_add_cart" method="post" enctype="multipart/form-data"><input type="hidden" name="store_product_id" value="38"><div class="control-group product-quantity"><label class="control-label" for="quantity">Quantity</label><div class="controls"><input type="number" name="quantity" class="input-mini" min="1" max="10" step="1" value="1" required=""></div></div><button type="submit" class="button alt btn btn-primary">Add to cart</button></form>');
+	return;
 }
 
+
+function the_product_gallery_thumbnails($thumnail_size = 'post-thumbnail', $image_size = 'medium', $zoom_image_size = 'large'){
+    global $post;
+    $post = get_post($post);
+
+    /* image code */
+    $thumblist = array();
+    //$images = & get_children('post_type=attachment&post_mime_type=image&output=ARRAY_N&orderby=menu_order&order=ASC&post_parent=' . $post->ID);
+    $args = array(
+        'numberposts' => -1,
+        'order' => 'ASC',
+        'orderby' => 'menu_order',
+        'post_mime_type' => 'image',
+        'post_parent' => $post->ID,
+        'post_type' => 'attachment',
+        'meta_query' => array(
+            array(
+                'key' => '_fik_product_image',
+                'value' => $post->ID,
+            )
+        ) // The attachment was downloaded via the Product Images metabox, so it should be shown in the image gallery
+    );
+
+    $images = & get_children($args);
+
+    if ($images) {
+        foreach ($images as $imageID => $imagePost) {
+            unset($the_thumbnail);
+            unset($the_image_url);
+            $the_thumbnail = wp_get_attachment_image($imageID, $thumnail_size, false);
+            $the_image = get_post($imageID);
+            $the_image_url = wp_get_attachment_image_src($imageID, $image_size, false);
+            $the_zoom_image_url = wp_get_attachment_image_src($imageID, $zoom_image_size, false);
+            $thumblist[$imageID] = '<a target="_blank" href="' . $the_image_url[0] . '" title="' . $the_image->post_title . '" data-width="' . $the_image_url[1] . '" data-height="' . $the_image_url[2] . '" data-zoom-image="' . $the_zoom_image_url[0] . '" data-zoomimagewidth="' . $the_zoom_image_url[1] . '" data-zoomimageheight="' . $the_zoom_image_url[2] . '">' . $the_thumbnail . '</a>';
+        }
+    }
+
+    if ($thumblist == array())
+        return false;
+
+    if ($hide_if_one) {
+        $thumbnail_count = 0;
+    }
+
+    $output = '<ul class="product-image-thumbnails thumbnails">';
+
+    foreach ($thumblist as $thumbnail) {
+        $output .= '<li class="thumbnail">' . $thumbnail . '</li>';
+        if ($hide_if_one) {
+            $thumbnail_count++;
+        }
+    }
+
+    $output .= '</ul>';
+
+    if ($hide_if_one) {
+        if ($thumbnail_count <= 1) {
+            return;
+        }
+    }
+
+    echo $output;
+    return;
+}
 
 ?>
