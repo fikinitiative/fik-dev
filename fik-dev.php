f0d800b18f77319319ded7d6f7e4d43e6c7a5c14
Bill Murphy <murphy@litio.net>
2014-03-17 16:56:59 +0100

    Add Product post type

--- fik-dev.php
+++ fik-dev.php
@@ -8,4 +8,52 @@
    Author URI: http://fikstores.com
    License: GPL2
    */
+
+// Register Product Post Type
+function product_post_type() {
+
+	$labels = array(
+		'name'                => _x( 'Products', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Product', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Post Type', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+		'all_items'           => __( 'All Items', 'text_domain' ),
+		'view_item'           => __( 'View Item', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'edit_item'           => __( 'Edit Item', 'text_domain' ),
+		'update_item'         => __( 'Update Item', 'text_domain' ),
+		'search_items'        => __( 'Search Item', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$args = array(
+		'label'               => __( 'fik_product', 'text_domain' ),
+		'description'         => __( 'Fik Stores - Product', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( ),
+		'taxonomies'          => array( 'store_category', 'post_tag' ),
+		'hierarchical'        => false,
+		'public'              => true,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'show_in_nav_menus'   => true,
+		'show_in_admin_bar'   => true,
+		'menu_position'       => 5,
+		'menu_icon'           => '',
+		'can_export'          => true,
+		'has_archive'         => true,
+		'exclude_from_search' => false,
+		'publicly_queryable'  => true,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'fik_product', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'product_post_type', 0 );
+
+
+
 ?>
