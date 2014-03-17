2226af7b1b4e5a3a6808ab55b4f519d88c6eedd0
Bill Murphy <murphy@litio.net>
2014-03-17 16:53:42 +0100

    Initial Fik Dev Plgugin commit

---
+++ fik-dev.php
@@ -0,0 +1,11 @@
+<?php
+   /*
+   Plugin Name: Fik Dev
+   Plugin URI: http://fikstores.com
+   Description: a plugin to help with the development of a theme for Fik Stores
+   Version: 1.0
+   Author: Fik Stores
+   Author URI: http://fikstores.com
+   License: GPL2
+   */
+?>
---
+++ readme.txt
@@ -0,0 +1,14 @@
+=== Fik Store Dev Plugin ===
+Tags: fik stores, dummy
+Requires at least: 3.8
+
+Fik Stores themers dev help
+
+== Installation ==
+Install this
+
+== Frequently Asked Questions ==
+FAQs
+
+== Screenshots ==
+1. Desc
