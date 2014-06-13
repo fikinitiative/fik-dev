63ba814548887e64027c0d3ece11d6efaf80b572
Bill Murphy <murphy@litio.net>
2014-06-13 20:28:07 +0200

    Store logo from administration

--- fik-dev.php
+++ fik-dev.php
@@ -260,7 +260,7 @@ function fik_messages($display = FALSE, $message = array ('error' => "This is a 
 
 function the_store_logo($size = "full", $args = array('class' => 'logo')){
     $logo_id = get_option('fik_store_logo');
-    echo wp_get_attachment_image($logo_id, $size, false, $args);
+    echo wp_get_attachment_image($logo_id['logo'], $size, false, $args);
 }
 
 // Store sections
@@ -296,6 +296,128 @@ function _store_sections_init() {
     ));
 }
 add_action('init', '_store_sections_init', 0);
+
+/**
+ * Register the settings to use on the theme options page
+ */
+add_action( 'admin_init', 'fik_register_settings' );
+
+/**
+ * Function to register the settings
+ */
+function fik_register_settings()
+{
+    // Register the settings with Validation callback
+    register_setting( 'store_logo', 'fik_store_logo', 'logo_validate_settings' );
+
+    // Add settings section
+    add_settings_section( 'fik_logo_section', 'Store Logo', 'fik_display_section', 'reading' );
+
+    // Create textbox field
+    $field_args = array(
+      'type'      => 'media_id',
+      'id'        => 'logo',
+      'name'      => 'logo_input',
+      'desc'      => 'Image ID from the <a href="./upload.php">media gallery</a>',
+      'std'       => '',
+      'label_for' => 'logo_input',
+      'class'     => 'css_class',
+      'option_name' => 'fik_store_logo'
+    );
+
+    add_settings_field( 'logo_textbox', 'Store logo image ID', 'fik_display_setting', 'reading', 'fik_logo_section', $field_args );
+}
+
+/**
+ * Function to add extra text to display on each section
+ */
+function fik_display_section($section){ 
+
+}
+
+/**
+ * Function to display the settings on the page
+ * This is setup to be expandable by using a switch on the type variable.
+ * In future you can add multiple types to be display from this function,
+ * Such as checkboxes, select boxes, file upload boxes etc.
+ */
+function fik_display_setting($args)
+{
+    extract( $args );
+    $options = get_option( $option_name );
+
+    if(!$options){
+      update_option( $option_name, '' );
+      $options = get_option( $option_name );
+    }
+
+    switch ( $type ) {  
+          case 'text':  
+              $options[$id] = stripslashes($options[$id]);  
+              $options[$id] = esc_attr( $options[$id]);  
+              echo "<input class='regular-text$class' type='text' id='$id' name='" . $option_name . "[$id]' value='$options[$id]' />";  
+              echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";  
+          break;  
+          case 'media_id':  
+              $options[$id] = stripslashes($options[$id]);  
+              $options[$id] = esc_attr( $options[$id]);  
+              echo "<input class='regular-text$class' type='number' id='$id' name='" . $option_name . "[$id]' value='$options[$id]' />";  
+              echo ($desc != '') ? "<br /><span class='description'>$desc</span>" : "";  
+          break;  
+    }
+}
+
+/**
+ * Callback function to the register_settings function will pass through an input variable
+ * You can then validate the values and the return variable will be the values stored in the database.
+ */
+function logo_validate_settings($input)
+{
+  foreach($input as $k => $v)
+  {
+    $newinput[$k] = trim($v);
+    
+    // Check the input is a letter or a number
+    if(!preg_match('/^[A-Z0-9 _]*$/i', $v)) {
+      $newinput[$k] = '';
+    }
+  }
+
+  return $newinput;
+}
+
+
+
+
+add_action('admin_menu', 'add_admin_menu');
+
+function add_admin_menu(){
+     add_menu_page( 'Options - Fik Theme DEV', 'Fik Theme DEV', 'manage_options', 'fikdev', 'fik_admin_function', $icon_url, $position ); 
+ 
+}
+
+function fik_admin_function(){
+  ?>
+    <div class="section panel">
+      <h1>Fik Theme DEV Options</h1>
+      <form method="post" enctype="multipart/form-data" action="options.php">
+        <?php 
+          settings_fields('store_logo'); 
+        
+          do_settings_sections('reading');
+        ?>
+            <p class="submit">  
+                <input type="submit" class="button-primary" value="<?php _e('Save Changes') ?>" />  
+            </p>  
+            
+      </form>
+
+    </div>
+    <?php
+}
+
+
+
 
 class store_sections_widget extends WP_Widget {
 
